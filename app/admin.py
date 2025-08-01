129378ba95872449fd3f576d4f4e2d29a6c42aae
Bobomurod2004 <goibnazarov_b@nuu.uz>
2025-08-01 18:34:33 +0500

    new1;08;25 18:34

---
+++ app/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from . import models
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    fields = 'create_at', 'title', 'description', 'project_image','status', 'git_link', 'link' ,'technology_stack'
+
+# admin.site.register(models.Project)
+admin.site.register(models.TechnologyStack)
+admin.site.register(models.Skill)
+admin.site.register(models.Me)
+admin.site.register(models.Article)
+admin.site.register(models.AboutMe)
+admin.site.register(models.Comment)
+admin.site.register(models.Experience)
+
---
+++ app/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+
+class Article(models.Model):
+    ARTICLE_CHOICES = [
+        ('a joke',"hazil"),
+        ('information',"Ma'lumot"),
+        ('fact',"Fakt")
+    ]
+    title = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField()
+    create_at = models.DateTimeField(auto_created=True)
+    status = models.CharField(max_length=200, choices=ARTICLE_CHOICES)
+
+class Me(models.Model):
+    image = models.ImageField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    git_link = models.URLField()
+    linkedin_link = models.URLField()
+    cv_file = models.FileField()
+    resume = models.FileField()
+
+class AboutMe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+class TechnologyStack(models.Model):
+    name = models.CharField(max_length=100, null=True ,blank=True)
+
+class Project(models.Model):
+
+    PROJECT_CHOICES = [
+        ("code","Kod"),
+        ("web_api","WEB API"),
+        ('rest_api',"REST API"),
+        ("fast_api","FAST API")
+    ]
+
+    create_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField()
+    project_image = models.ImageField()
+    status = models.CharField(max_length=100, choices=PROJECT_CHOICES, null=True, blank=True)
+    git_link = models.URLField()
+    link = models.URLField()
+    technology_stack = models.ManyToManyField(
+        TechnologyStack, related_name='project_stack',
+
+    )
+
+class Skill(models.Model):
+    key = models.CharField(max_length=100, null=True, blank=True)
+    value = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.key} - {self.value}%"
+
+class Comment(models.Model):
+    full_name = models.CharField(max_length=100)
+    comment = models.TextField()
+    create_at = models.DateTimeField(auto_now=True)
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    link = models.URLField()
+    date = models.DateTimeField(auto_now=True)
+    text = models.TextField()
